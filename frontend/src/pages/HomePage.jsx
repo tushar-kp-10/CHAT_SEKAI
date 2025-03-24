cd14ebcb8cff8f69a93383509f20fdc13ac0d9b3
Tushar Kanta Padhiary <tusharkp2002@gmail.com>
2025-03-24 22:48:11 +0530

    C5

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,14 +6,14 @@ import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
-  const { selectedUSer} = useChatStore();
+  const { selectedUser} = useChatStore();
   return (
     <div className='!bg-bas-200'>
       <div className='flex items-center !justify-center !pt-20 !px-4'>
         <div className='!bg-base-100 !rounded-lg !shadow-cl !w-full !max-2-6xl !h-[calc(100vh-8rem)]'>
           <div className='!flex !h-full !rounded-lg !overflow-hidden'>
             <Sidebar/>
-            {!selectedUSer ? <NoChatSelected/> : <ChatContainer/>}
+            {!selectedUser ? <NoChatSelected/> : <ChatContainer/>}
           </div>
         </div>
       </div>
